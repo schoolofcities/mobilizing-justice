b63ca9d867bfc384a4856e0a2ab84130c7af104a
Jeff Allen <jeffallen89@gmail.com>
2026-04-17 14:47:58 -0400

    static favicon nojeykyll, build

---
+++ docs/_app/immutable/chunks/B4MzPIEk.js
@@ -0,0 +1,1 @@
+import{c as t,a as c}from"./D7melqRb.js";import{j as i,E as p,k as m}from"./BfFoFcZm.js";import{B as f}from"./DLgSlEkw.js";function l(n,r,...a){var o=new f(n);i(()=>{const e=r()??null;o.ensure(e,e&&(s=>e(s,...a)))},p)}function E(n,r){var a=t(),o=m(a);l(o,()=>r.children),c(n,a)}export{E as L};
